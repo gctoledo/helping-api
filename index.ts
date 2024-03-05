fceccea51dbf6e3f6aee4529e98fd593f6b880f6
Gabriel <gabrielcostatoledo@outlook.com>
2024-03-05 13:55:47 -0300

    feat: add POST route to create users

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { Request, Response } from "express";
 
 import { GetUserByIdController } from "./src/controller/get-user-by-id";
+import { CreateUserController } from "./src/controller/create-user";
 
 const app = express();
 
@@ -12,6 +13,14 @@ app.get("/api/users/:userId", async (req: Request, res: Response) => {
   const getUserByIdController = new GetUserByIdController();
 
   const response = await getUserByIdController.execute(req);
+
+  res.status(response.statusCode).send(response.body);
+});
+
+app.post("/api/users", async (req: Request, res: Response) => {
+  const createUserController = new CreateUserController();
+
+  const response = await createUserController.execute(req);
 
   res.status(response.statusCode).send(response.body);
 });
