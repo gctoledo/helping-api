e15557a294849edb4b612230ab7e385b98600b70
Gabriel <gabrielcostatoledo@outlook.com>
2024-03-05 13:54:31 -0300

    feat: add helpers to response to http request

---
+++ src/controller/helpers/http.ts
@@ -0,0 +1,21 @@
+import {
+  BodyParams,
+  ControllerReturnTypes,
+} from "../types/controller-return-types";
+
+export const serverError = (): ControllerReturnTypes => ({
+  statusCode: 500,
+  body: {
+    message: "Internal server error",
+  },
+});
+
+export const badRequest = (body: BodyParams): ControllerReturnTypes => ({
+  statusCode: 400,
+  body,
+});
+
+export const ok = (body: BodyParams) => ({
+  statusCode: 200,
+  body,
+});
